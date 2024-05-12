69758543c812f9fd1801a52f277aed9e55936d2f
Girey0211 <101105181+Girey0211@users.noreply.github.com>
2024-05-13 08:25:36 +0900

    refactor: main.py

--- app/main.py
+++ app/main.py
@@ -2,21 +2,16 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
 from app.controller.riot.riot_controller import riot_controller
+from app.controller.summoner.summoner_controller import summoner_controller
 from app.controller.user.user_controller import user_controller
 from app.domain.restapi.tables import Base
 from app.database import engine
-from app.domain.graphql.schema import graphql_app
 
 
 app = FastAPI()
-
-origins = [
-    "*"
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -25,11 +20,10 @@ app.add_middleware(
 Base.metadata.create_all(bind=engine)
 
 
-
 @app.get("/hello/{name}", description="테스트 api")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 app.include_router(user_controller)
 app.include_router(riot_controller)
-app.include_router(graphql_app, prefix="/graphql")
+app.include_router(summoner_controller, prefix="/graphql", tags=["graphql"])
